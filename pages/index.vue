569a99cdfb16d9d21d1edcbf450c17ab86986c0b
Alfian An-Naufal Nuha <alfian.nuha@gmail.com>
2021-10-08 20:31:47 +0700

    Initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -106,6 +106,17 @@
                   width="100%"
                   height="100%"
                   :src="require('@/assets/image/profile_.png')">
+                  <template v-slot:placeholder>
+                    <v-row
+                      class="fill-height ma-0"
+                      align="center"
+                      justify="center">
+                      <v-progress-circular
+                        indeterminate
+                        color="grey lighten-5">
+                      </v-progress-circular>
+                    </v-row>
+                  </template>
                 </v-img>
               </v-card>
 
